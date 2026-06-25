ef5618a0aabd69d83a8e8d1760f4293596c12531
Alexey Flyagin <aiflyagin@stud.kantiana.ru>
2026-06-25 14:22:12 +0300

    Add MainWindow layout and base infrastructure

---
+++ Taskish/Commands/RelayCommandT.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace Taskish.Commands
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool>? _canExecute;
+
+        public RelayCommand(Action<T> execute, Func<T, bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) =>
+            parameter is T t ? _canExecute?.Invoke(t) ?? true : true;
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is T t) _execute(t);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ Taskish/Views/TitleBar.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Taskish.Views
+{
+    public partial class TitleBar : UserControl
+    {
+        private DispatcherTimer? _timer;
+
+        public TitleBar()
+        {
+            InitializeComponent();
+            StartClock();
+        }
+
+        private void StartClock()
+        {
+            UpdateTime();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (s, e) => UpdateTime();
+            _timer.Start();
+        }
+
+        private void UpdateTime()
+        {
+            TimeTextBlock.Text = DateTime.Now.ToString("HH:mm");
+            DateTextBlock.Text = DateTime.Now.ToString("ddd, d MMM yyyy", CultureInfo.CurrentCulture);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window.WindowState = window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+    }
+}
